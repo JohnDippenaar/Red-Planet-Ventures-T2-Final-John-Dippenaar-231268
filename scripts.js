c09a96f444b18f25d013d3325e6e26c6c10dd2fd
John Dippenaar <231268@virtualwindow.co.za>
2024-06-13 10:56:30 +0200

    Update
    Added everything I have done and created this new file for this term's submission.

---
+++ scripts.js
@@ -0,0 +1,81 @@
+function decreaseMars() {
+  let ticketsMars = document.getElementById("ticketsMars");
+  if (ticketsMars.value != 0) {
+    ticketsMars.value = parseInt(ticketsMars.value) - 1;
+  } else {
+    ticketsMars.value = 0;
+  }
+}
+
+function increaseMars() {
+  let ticketsMars = document.getElementById("ticketsMars");
+  ticketsMars.value = parseInt(ticketsMars.value) + 1;
+}
+
+function decreaseNeptune() {
+  let ticketsNeptune = document.getElementById("ticketsNeptune");
+  if (ticketsNeptune.value != 0) {
+    ticketsNeptune.value = parseInt(ticketsNeptune.value) - 1;
+  } else {
+    ticketsNeptune.value = 0;
+  }
+}
+
+function increaseNeptune() {
+  let ticketsNeptune = document.getElementById("ticketsNeptune");
+  ticketsNeptune.value = parseInt(ticketsNeptune.value) + 1;
+}
+function decreaseJupiter() {
+  let ticketsJupiter = document.getElementById("ticketsJupiter");
+  if (ticketsJupiter.value != 0) {
+    ticketsJupiter.value = parseInt(ticketsJupiter.value) - 1;
+  } else {
+    ticketsJupiter.value = 0;
+  }
+}
+
+function increaseJupiter() {
+  let ticketsJupiter = document.getElementById("ticketsJupiter");
+  ticketsJupiter.value = parseInt(ticketsJupiter.value) + 1;
+}
+
+function decreaseSaturn() {
+  let ticketsSaturn = document.getElementById("ticketsSaturn");
+  if (ticketsSaturn.value != 0) {
+    ticketsSaturn.value = parseInt(ticketsSaturn.value) - 1;
+  } else {
+    ticketsSaturn.value = 0;
+  }
+}
+
+function increaseSaturn() {
+  let ticketsSaturn = document.getElementById("ticketsSaturn");
+  ticketsSaturn.value = parseInt(ticketsSaturn.value) + 1;
+}
+
+function decreaseUranus() {
+  let ticketsUranus = document.getElementById("ticketsUranus");
+  if (ticketsUranus.value != 0) {
+    ticketsUranus.value = parseInt(ticketsUranus.value) - 1;
+  } else {
+    ticketsUranus.value = 0;
+  }
+}
+
+function increaseUranus() {
+  let ticketsUranus = document.getElementById("ticketsUranus");
+  ticketsUranus.value = parseInt(ticketsUranus.value) + 1;
+}
+function decreaseMoonlight() {
+  let ticketsMoonlight = document.getElementById("ticketsMoonlight");
+  if (ticketsMoonlight.value != 0) {
+    ticketsMoonlight.value = parseInt(ticketsMoonlight.value) - 1;
+  } else {
+    ticketsMoonlight.value = 0;
+  }
+}
+
+function increaseMoonlight() {
+  let ticketsMoonlight = document.getElementById("ticketsMoonlight");
+  ticketsMoonlight.value = parseInt(ticketsMoonlight.value) + 1;
+}
